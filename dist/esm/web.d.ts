7337dd9238fee518b4ecea851dd5f14868c69421
marhano <carullomarjan@gmail.com>
2025-02-27 13:58:28 +0800

    .updated dist

---
+++ dist/esm/web.d.ts
@@ -0,0 +1,19 @@
+import { WebPlugin } from '@capacitor/core';
+import type { AutoPayPlugin } from './definitions';
+export declare class AutoPayWeb extends WebPlugin implements AutoPayPlugin {
+    checkOverlayPermission(): Promise<any>;
+    enableOverlayPermission(): Promise<any>;
+    checkAccessibility(): Promise<any>;
+    enableAccessibility(): Promise<any>;
+    stopNavigation(options: any): Promise<any>;
+    performGesture(options: {
+        x: number;
+        y: number;
+    }): Promise<any>;
+    navigateGCash(options: any): Promise<any>;
+    echo(options: {
+        value: string;
+    }): Promise<{
+        value: string;
+    }>;
+}
